b972547f0a16983ee9b5d73c5166a06d06017092
DevendraK25 <kulk.devendra@gmail.com>
2023-03-22 17:41:06 -0400

    Added scss stuff

---
+++ src/modules/landing-page/components/landing-page-howtoplay-component/landing-page-howtoplay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './landing-page-howtoplay.scss';
+
+export const LandingPageHowToPlay: React.FC = () => {
+  return(
+    <div className="landingPageHowToPlay">
+        To play Wonky Chess, it is white to move!<br></br>
+        <b>Caution:</b> Your opponent may be a little wonky!
+    </div>
+  )
+}
